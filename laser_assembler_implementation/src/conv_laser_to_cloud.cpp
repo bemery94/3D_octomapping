cc48baebff966578e2800abb70342a3d254d305c
Brendan Emery <b.emery94@gmail.com>
2016-02-02 11:21:15 +1100

    Finish conv_laser_to_cloud node.
    Node is now fully functional.

--- laser_assembler_implementation/src/conv_laser_to_cloud.cpp
+++ laser_assembler_implementation/src/conv_laser_to_cloud.cpp
@@ -20,26 +20,24 @@
 void laserScanCallBack(const sensor_msgs::LaserScan::ConstPtr&);
 
 ros::Publisher point_cloud_pub;
+tf::TransformListener* listener_ = NULL;
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "conv_laser_to_cloud");
     ros::NodeHandle n;
 
-    ros::Duration test;
-    test.sec = 100.0;
-    tf::TransformListener listener_(test);
+    listener_ = new tf::TransformListener(ros::Duration(100));
 
     // Subscribers
     ros::Subscriber laser_scan_sub = n.subscribe<sensor_msgs::LaserScan>
                                       ("laser_scan_in", 100, laserScanCallBack);
 
-//     Publishers
+    // Publishers
     point_cloud_pub = n.advertise<sensor_msgs::PointCloud>("point_cloud_out", 100);
 
     ros::spin();
 }
-
 
 /*
     Callback to convert the LaserScan message to PointCloud and publish the PointCloud message.
@@ -49,46 +47,21 @@ void laserScanCallBack(const sensor_msgs::LaserScan::ConstPtr& scan_in)
     sensor_msgs::PointCloud cloud;
     laser_geometry::LaserProjection projector_;
 
-//    while(!listener_.canTransform(scan_in->header.frame_id, "/map_world_frame", ros::Time(0)) && ros::ok())
-//        {
-//        ros::Duration(1).sleep();
-//        if(listener_.canTransform(scan_in->header.frame_id, "/map_world_frame", ros::Time(0)))
-//        break;
-//        ros::Duration(1).sleep();
-//        if(listener_.canTransform(scan_in->header.frame_id, "/map_world_frame", ros::Time(0)))
-//        break;
-//        ros::Duration(1).sleep();
-//        if(listener_.canTransform(scan_in->header.frame_id, "/map_world_frame", ros::Time(0)))
-//        break;
-//        ros::Duration(1).sleep();
-//        ROS_INFO("Cannot retrieve thruster TF's! Ensure that the TF publisher\
-//                  node is running");
-//        }
 
-    ROS_INFO_STREAM(ros::Time::now());
-    ROS_INFO_STREAM(scan_in->header.stamp);
-    ROS_INFO_STREAM(ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment));
-    std::cout << "outside while loop" << std::endl;
-    if(!listener_.waitForTransform(
-        "/base_link",
-        //scan_in->header.frame_id,
-        "/laser_lsl",
-        scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment) + ros::Duration(0.3),
+    if(!listener_->waitForTransform(
+        scan_in->header.frame_id,
+        "/map_world_frame",
+        scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment),
         ros::Duration(5.0)))
         {
-            ROS_INFO_STREAM(ros::Time::now());
-            std::cout << "Exiting" << std::endl;
+            std::cout << "Cannot find TF between " << scan_in->header.frame_id <<
+                            "and map_world_frame" << std::endl;
             return;
         }
-//        projector_.transformLaserScanToPointCloud ("/base_link", *scan_in, cloud, listener_);
+        projector_.transformLaserScanToPointCloud ("/map_world_frame", *scan_in, cloud, *listener_);
 
-//    ROS_INFO_STREAM(listener_.frameExists("/map_world_frame"));
+    std::cout << "Converted LaserScan to PointCloud from " << scan_in->header.frame_id << "to ==> "
+              << "map_world_frame" << std::endl;
 
-    std::cout << "After" << std::endl;
-
-
-
-    std::cout << "Converted LaserScan to PointCloud" << std::endl;
     point_cloud_pub.publish(cloud);
-
 }
