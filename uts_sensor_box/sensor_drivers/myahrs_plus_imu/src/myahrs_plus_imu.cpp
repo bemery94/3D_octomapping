15bcc9a384361c5bc9377d14405ac1d3628d7571
Brendan Emery <b.emery94@gmail.com>
2016-02-10 15:58:37 +1100

    Remove euler angle printing from imu node

--- uts_sensor_box/sensor_drivers/myahrs_plus_imu/src/myahrs_plus_imu.cpp
+++ uts_sensor_box/sensor_drivers/myahrs_plus_imu/src/myahrs_plus_imu.cpp
@@ -54,9 +54,6 @@ public:
 	imu_data_.header.frame_id = "imu";
 	imu_data_.header.stamp = ros::Time::now();
 
-    ROS_INFO_STREAM("euler angles");
-    ROS_INFO_STREAM(e);
-
 	myahrs_pub.publish(imu_data_);
     }
 
@@ -86,7 +83,6 @@ int main(int argc, char **argv)
 	ros::init(argc,argv, "myahrs_node");
 	ros::NodeHandle nh;
 	myahrs_pub = nh.advertise<sensor_msgs::Imu>(imu_topic_, 1);
-	myahrs_euler_pub = nh.advertise<sensor_msgs::Imu>("imu_euler", 1);
 
 	nh.param<std::string>("imu_port", serial_device, "/dev/sb/imu");
 
